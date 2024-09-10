a73a8e9fcd9b2d2f0ff28f7210682e39f76d9144
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-10 14:01:27 -0300

    Aula 03.2 - Link Prefetch

---
+++ aula03/a3.2/pages/prefetch.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+
+export default function PrefetchScreen() {
+  return (
+    <>
+      <h1>Prefetch Page!</h1>
+
+      <div>
+        <ul>
+          <li>
+            {/** Default: True */}
+            {/** True: faz um pré-download da página do Link */}
+            {/** False: não faz o download porém quando é feito o hover no link ele faz o pré-download */}
+            <Link href="/" prefetch={false}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" prefetch={false}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/prefetch" prefetch={false}>
+              Prefetch
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
