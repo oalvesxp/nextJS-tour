0b208579a93abb77ecd7b14b71ea8a684cc5ef50
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-07 21:12:24 -0300

    Aula 05.05 - Estilizando o UserTable

--- aula05/pages/index.js
+++ aula05/pages/index.js
@@ -1,9 +1,11 @@
 import { SignupForm } from '../src/components/SignupForm'
+import { UserTable } from '../src/components/UserTable'
 
 export default function HomePage() {
   return (
     <>
       <SignupForm />
+      <UserTable />
     </>
   )
 }
